6af42efcaa633c02361b415647de064505aed866
ujinTheFroggy <eugenefronzak@gmail.com>
2018-06-15 00:05:31 +0500

    Task2

---
+++ task2.rb
@@ -0,0 +1,26 @@
+#Task 2
+
+#Put your code here
+def fibo(a)
+    target = a.abs
+    if a == 0 || a==1 
+        return a
+    end
+    first = 0
+    second = 1
+    i=1
+    res=0
+    while i != target
+        i+=1
+        res = first + second
+        first = second
+        second = res
+    end
+    if a<0
+        return ((-1)**(a+1))*res
+    end
+    return res
+end
+
+input = ARGV[0].to_i
+puts fibo(input)
